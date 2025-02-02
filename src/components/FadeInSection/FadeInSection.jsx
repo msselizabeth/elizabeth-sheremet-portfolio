153eb3195d5969340d9ee31fdfb5731e55bf2a04
Elizabeth <lizagotovitryby@gmail.com>
2025-02-02 10:04:58 -0500

    created lazy frame

---
+++ src/components/FadeInSection/FadeInSection.jsx
@@ -0,0 +1,40 @@
+import { useRef, useState, useEffect } from 'react';
+import './FadeInSection.scss'; 
+
+function FadeInSection({ children }) {
+  const domRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+       
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    if (domRef.current) {
+      observer.observe(domRef.current);
+    }
+
+    return () => {
+      if (domRef.current) {
+        observer.unobserve(domRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      ref={domRef}
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default FadeInSection;
