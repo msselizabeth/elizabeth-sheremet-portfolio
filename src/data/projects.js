6abfb0dae43e80e582353b3dd03d467568b842d5
Elizabeth <lizagotovitryby@gmail.com>
2025-02-04 21:28:53 -0500

    added more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,6 +11,17 @@ export const projects = [
             "GastroGuide is an innovative, multilingual web application that brings the world of cooking and food culture to your fingertips. GastroGuide stands out with its personalized features: users can create profiles to receive tailored recipe suggestions, save and rate their favorites, chat with an AI assistant for recommendations, and participate in community discussions. It’s more than just a recipe app—GastroGuide is your interactive and personalized guide to global cuisine and healthy living.",
     },
     {
+        title: "Katya Rybalka",
+        image: "",
+        alt: "Katya Rybalks - food and fishing blogger in Ukraine",
+        video: "/katya-demo.webm",
+        github: "",
+        link: "https://katya-rybalka.vercel.app/",
+        techStack: ["js","react", "next", "css", "sass", "vercel"],
+        description:
+            "Developed a modern and responsive personal website for a blogger, designed to showcase key information, achievements, and contact details. The website includes a dedicated section for personal background and a contact form for business inquiries and advertising opportunities. Implemented automatic dark and light mode switching based on the user's device settings for enhanced accessibility and usability.",
+    },
+    {
         title: "Iggy Discover",
         image: "/images/iggy.png",
         alt: "Iggy Discover - AI search tool",
@@ -31,5 +42,16 @@ export const projects = [
         techStack: ["js", "react", "css", "sass", "vite"],
         description:
             "Culinary Notes is a single-page application where users can explore recipes for a festive New Year’s table created by renowned chefs. The application also allows users to contact a manager for consultation or placing an order and preview the electronic version of the cookbook to get an idea of its content and design.",
+    },
+    {
+        title: "E-commerce",
+        image: "",
+        alt: "E-commerce",
+        video: "/e-com-demo.webm",
+        github: "",
+        link: "",
+        techStack: ["ts", "react", "next", "tailwindcss", "mysql", "prisma", "aws", "nextauth"],
+        description:
+            "Developed a dynamic e-commerce website with a flexible product catalog, featuring pagination, filtering, and sorting for easy navigation. Implemented secure user authentication and an intuitive cart and checkout system for a smooth shopping experience. Designed with a fully responsive interface, ensuring accessibility and usability across all devices.",
     },
 ];
