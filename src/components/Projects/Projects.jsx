cdd23545bc47916d54dadd0a244429f82c9f993a
Elizabeth <lizagotovitryby@gmail.com>
2025-02-12 14:10:55 -0500

    added pagination

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,20 +3,28 @@ import { projects } from "../../data/projects";
 import ProjectItem from "../ProjectItem/ProjectItem";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import FadeInSection from "../FadeInSection/FadeInSection";
+import { useState } from "react";
+import LoadMore from "../Buttons/LoadMore";
 
 
 export default function Projects() {
+  const [visibleProjects, setVisibleProjects] = useState(2);
+
+  const loadMore = () => {
+    setVisibleProjects(prev => prev + 1);
+  }
   return (
     <>
       <SectionTitle title="Projects" />
       <section className="section projects" id="projects">
         <ul className="projects__list">
-      {projects.map((project, index) => (
+      {projects.slice(0, visibleProjects).map((project, index) => (
         <FadeInSection key={index} >
           <ProjectItem project={project} index={index} />
        </FadeInSection>
       ))}
-    </ul>
+        </ul>
+        {visibleProjects < projects.length && <LoadMore loadMore={loadMore}/>}
       </section>
     </>
   );
